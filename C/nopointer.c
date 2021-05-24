619867bbe0bf06ad20451eb3b9bf10bc1d826100
Yaswanth Reddy <yaswanthreddy@Yaswanths-MacBook-Pro.local>
2021-05-24 18:35:25 +0530

    nopointer

---
+++ C/nopointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	//int d;
+	//float r;
+	char n;
+	//scanf("%d", &d);
+	//printf("i: %d \n", d);
+
+	//scanf("%f", &r);
+	//printf("f: %f \n", r);
+
+	scanf("%c", &n);
+	printf("c: %c \n", n);
+}
